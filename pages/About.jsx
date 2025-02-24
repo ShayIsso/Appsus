9281c29748dbc8f0144fc38d737132af03eacbb9
ShayIsso <shayisso@gmail.com>
2025-02-24 10:54:46 +0200

    Fix image paths in About page

--- pages/About.jsx
+++ pages/About.jsx
@@ -3,12 +3,12 @@ export function About() {
         <section className="container-about">
             <div className="team-members">
                 <div className="member">
-                    <img className="member-img" src="../assets/img/shay.jpg" alt="Shay Isso" />
+                    <img className="member-img" src="assets/img/shay.jpg" alt="Shay Isso" />
                     <h3 className="member-name">Shay Isso</h3>
                     <p className="member-overview">Passionate Full-Stack Developer, always eager to learn and build. Focused on clean code, problem-solving, and creating impactful solutions.</p>
                 </div>
                 <div className="member">
-                    <img className="member-img" src="../assets/img/stav.jpg" alt="Stav Ohana" />
+                    <img className="member-img" src="assets/img/stav.jpg" alt="Stav Ohana" />
                     <h3 className="member-name">Stav Ohana</h3>
                     <p className="member-overview">Full-Stack Developer, driven by learning and creating meaningful solutions.</p>
                 </div>
