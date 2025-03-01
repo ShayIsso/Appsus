c2a5b52e34ceb19510dbd8a77033e21d0cfa0525
ShayIsso <shayisso@gmail.com>
2025-03-01 14:45:18 +0200

    Add sample email data for testing in mail service

--- apps/mail/services/mail.service.js
+++ apps/mail/services/mail.service.js
@@ -2,6 +2,17 @@ import { utilService } from '../../../services/util.service.js'
 import { storageService } from '../../../services/async-storage.service.js'
 
 const MAIL_KEY = 'mailDB'
+
+const mail1 = {
+    createdAt: Date.now() - 2000,
+    subject: 'Get Instant Feedback on your challenges',
+    body: 'Hello Shay Isso! Over the years, many users have asked for help, guidance, and insights on their challenge solutions. So today, were excited to announce our AI-powered code review feature. Were calling it Instant Feedback',
+    isRead: false,
+    sentAt: 1551133930594,
+    removedAt: null,
+    from: 'shayniso@gmail.com',
+    to: 'user@appsus.com'
+}
 _createMails()
 
 export const mailService = {
@@ -40,6 +51,7 @@ function _createMails() {
         mail.push(_createMail('shay@gmail.com'))
         mail.push(_createMail('david@gmail.com'))
         mail.push(_createMail('ilana@gmail.com'))
+        mail.push(mail1)
         mail.push(_createMail('or@gmail.com'))
         utilService.saveToStorage(MAIL_KEY, mail)
     }
